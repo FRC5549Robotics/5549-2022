530b93d5a5d7237a0223f751b0ccbc853976373f
Fernandez <1592663@fcps.edu>
2022-02-03 16:58:36 -0500

    added to auton

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants;
@@ -16,17 +17,23 @@ public class Auto extends CommandBase {
   /** Creates a new Auto. */
   Drivetrain m_drivetrain;
   Limelight m_limelight;
+  Shooter m_shooter;
+  Indexer m_indexer;
   private double m_time;
   private double startTime;
   boolean myAutoFinished = false;
-  private double m_maxTime;
+  private double m_maxTime_drive;
+  private double m_maxTime_index;
   Double m_angle2;
 
-  public Auto(Drivetrain drivetrain, Limelight limelight, double time) {
+  public Auto(Drivetrain drivetrain, Limelight limelight, double timeDrive, double timeIndedxer, Shooter shooter, Indexer indexer) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_drivetrain = drivetrain;
     m_limelight = limelight;
-    m_maxTime = time;
+    m_maxTime_drive = timeDrive;
+    m_maxTime_index = timeIndedxer;
+    m_shooter = shooter;
+    m_indexer = indexer;
     m_angle2 = m_limelight.getAngle();
     addRequirements(drivetrain, limelight);
   }
@@ -43,22 +50,27 @@ public class Auto extends CommandBase {
   @Override
   public void execute() {
     m_time = (System.currentTimeMillis() - startTime) / 1000;
-    if ((m_time >= 0.0) && (m_time < m_maxTime)) {
+    if ((m_time >= 0.0) && (m_time < m_maxTime_drive)) {
       // Constants are made positive because robot is supposed to go backwards. Since autoDrive already makes the values
       // negative, negatives here would cancel it out.
       m_drivetrain.autoDrive(Constants.DRIVE_AUTO_SPEED, Constants.DRIVE_AUTO_SPEED);
     } 
-    if ((m_time >= m_maxTime)){
+    if ((m_time >= m_maxTime_drive)){
       if (m_limelight.getAngle() > 2 || m_limelight.getAngle() < -2) {
         m_drivetrain.arcadeDriveMethod(m_angle2);
       }
       else{
-        myAutoFinished = true;
+        m_shooter.on();
+        startTime = System.currentTimeMillis();
+        m_time = (System.currentTimeMillis() - startTime) / 1000;
+        if ((m_time >= 0.0) && (m_time < m_maxTime_index)){
+          m_indexer.indexer_run(Constants.INDEXER_SPEED);
+        }
+        if ((m_time >= m_maxTime_index)){
+          myAutoFinished = true;
+        }
       }
     }
-
-
-
   }
 
   // Called once the command ends or is interrupted.
